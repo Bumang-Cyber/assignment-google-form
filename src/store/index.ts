2d5d94982c2270f465711503baf614f44a09d763
Bumang-Cyber <jove0729@naver.com>
2024-03-15 18:28:25 +0900

    feat: 초기 세팅 완료

---
+++ src/store/index.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import levelSlice from "./levelSlice";
+import playingStateSlice from "./playingStateSlice";
+import goalAmountSlice from "./goalAmountSlice";
+import mineLeftSlice from "./mineLeftSlice";
+import modalSlice from "./modalSlice";
+import timeSlice from "./timeSlice";
+
+const store = configureStore({
+  reducer: {
+    levels: levelSlice.reducer,
+    playing: playingStateSlice.reducer,
+    goalAmount: goalAmountSlice.reducer,
+    mineLeft: mineLeftSlice.reducer,
+    modal: modalSlice.reducer,
+    time: timeSlice.reducer,
+  },
+});
+
+// store의 reducer들의 현재 상태의 type
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
---
+++ src/store/levelSlice.ts
@@ -0,0 +1,29 @@
+import { LevelValueType } from "./../types/level";
+import { createSlice } from "@reduxjs/toolkit";
+
+type InitialState = {
+  value: LevelValueType;
+};
+
+const initialState: InitialState = {
+  // constant에 모든 레벨 상수화
+  value: {
+    TITLE: "Beginner",
+    X: 8,
+    Y: 8,
+    MINE: 10,
+  },
+};
+
+const levelSlice = createSlice({
+  name: "level",
+  initialState,
+  reducers: {
+    change: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export default levelSlice;
+export const { change } = levelSlice.actions;
